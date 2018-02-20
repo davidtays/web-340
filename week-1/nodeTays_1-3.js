c5c41e369a72cd2aac1726be91c96c30f1ca36b6
David Tays <davidtays22@gmail.com>
2018-02-19 19:27:09 -0600

    added header

--- week-1/nodeTays_1-3.js
+++ week-1/nodeTays_1-3.js
@@ -1,5 +1,21 @@
+var header = require('../header.js');
+/*
+============================================
+; Title:  Assignment 1-3
+; Author: David Tays
+; Date:   28 January 2018
+; Modified By: <David Tays>
+;
+;===========================================*/
 var url = require("url");
 var parsedURL = url.parse("https://davidtays.github.io/WebPortfolio/profile?name=tays");
+
+//Header
+console.log('\n'); 
+console.log(header.display("David", "Tays", "Excercise 1.3") + "\n");
+
+
+//output
 console.log(parsedURL.protocol);
 console.log(parsedURL.host);
 console.log(parsedURL.query);
