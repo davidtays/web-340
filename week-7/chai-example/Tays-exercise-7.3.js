363e1516f1eb5611d0c3654f60dd576762541980
David Tays <davidtays22@gmail.com>
2018-04-05 20:32:01 -0500

    updating attribution in base class

--- week-7/chai-example/Tays-exercise-7.3.js
+++ week-7/chai-example/Tays-exercise-7.3.js
@@ -1,3 +1,12 @@
+/*
+============================================
+; Title:  Assignment 7-3
+; Author: David Tays
+; Date:   April 4 2018
+; Modified By: <David Tays>
+;Description: Mocha-Chai
+;===========================================*/
+
 function flies(str){
     return str.split(",");
 }
